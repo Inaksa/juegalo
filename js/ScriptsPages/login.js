73b579cca5a4e87052c3db6b2a7ffb98a015dd9a
Alex German <alexgerman@gmail.com>
2024-05-28 16:06:44 -0300

    Implemented validations in login and register

---
+++ js/ScriptsPages/login.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+
+    document.getElementById("submitLogin").addEventListener("click", function () {
+        if (!validateForm()) {
+            console.log('El formulario no es válido. Por favor, corrige los errores.');
+            event.preventDefault(); // Evita que el formulario se envíe si hay errores de validación
+        } else {
+            console.log('El formulario es válido. Enviar datos...');
+        }
+    });
+
+    // Función para validar todo el formulario
+    const validateForm = () => {
+        let isValid = true;
+        isValid = validateEmailField('user', 'El correo electrónico no es válido') && isValid;
+        isValid = validatePassField('pass', 'La contraseña es obligatoria') && isValid;
+        return isValid;
+    };
+
+    const validatePassField = (fieldId, errorMessage) => {
+        const field = document.getElementById(fieldId);
+        const errorField = document.getElementById('errortext-pass');
+        const value = field.value.trim();
+
+        if (value === '') {
+            errorField.innerText = "La contraseña es obligatoria";
+            return false;
+        } else {
+            errorField.innerText = "";
+            return true;
+        }
+    };
+
+    const validateEmailField = (fieldId, errorMessage) => {
+        // Obtiene el elemento del campo de correo electrónico mediante su ID
+        const field = document.getElementById(fieldId);
+        // Obtiene el valor del campo y elimina los espacios en blanco al principio y al final
+        const email = field.value.trim();
+        const errorField = document.getElementById('errortext-user');
+        // Si el campo de correo electrónico está vacío
+        if (email === '') {
+            errorField.innerText = "El correo electrónico es obligatorio";
+            field.focus();
+            return false;
+        } else if (!isEmail(email)) {
+            errorField.innerText = "El email no es válido";
+            field.focus();
+            return false;
+        } else {
+            errorField.innerText = "";
+            return true;
+        }
+    };
+
+    // Función para validar si una cadena es una dirección de correo electrónico válida
+    const isEmail = (email) => {
+        // Expresión regular para validar el formato de correo electrónico
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        // Verifica si el correo electrónico cumple con el formato
+        return re.test(email);
+    };
+});
---
+++ js/ScriptsPages/register.js
@@ -0,0 +1,129 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("submitLogin").addEventListener("click", function () {
+        if (!validateForm()) {
+            console.log('El formulario no es válido. Por favor, corrige los errores.');
+            event.preventDefault(); // Evita que el formulario se envíe si hay errores de validación
+        } else {
+            console.log('El formulario es válido. Enviar datos...');
+        }
+    });
+
+    // Función para validar todo el formulario
+    const validateForm = () => {
+        let isValid = true;
+        
+        isValid = validateFirstNameField() && isValid;
+        isValid = validateLastNameField() && isValid;
+        isValid = validateUserField() && isValid;
+
+        isValid = validatePassField('La contraseña es obligatoria') && isValid;
+        isValid = validateConfirmPassField('Confirmar debería ser igual que tu contraseña') && isValid;
+        
+        isValid = validateAcceptTerms() && isValid;
+
+        return isValid;
+    };
+
+    const validateFirstNameField = () => {
+        const field = document.getElementById("fname");
+        const valor = field.value.trim();
+        const errorField = document.getElementById('errortext-nombre');
+        // Si el campo de correo electrónico está vacío
+        if (valor === '') {
+            errorField.innerText = "El nombre es obligatorio";
+            field.focus();
+            return false;
+        } else {
+            errorField.innerText = "";
+            return true;
+        }
+    };
+    const validateLastNameField = () => {
+        const field = document.getElementById("lname");
+        const valor = field.value.trim();
+        const errorField = document.getElementById('errortext-apellido');
+        if (valor === '') {
+            errorField.innerText = "El apellido es obligatorio";
+            field.focus();
+            return false;
+        } else {
+            errorField.innerText = "";
+            return true;
+        }
+    };
+    const validateUserField = () => {
+        const field = document.getElementById("user");
+        const valor = field.value.trim();
+        const errorField = document.getElementById('errortext-user');
+        // Si el campo de correo electrónico está vacío
+        if (valor === '') {
+            errorField.innerText = "El correo electrónico es obligatorio";
+            field.focus();
+            return false;
+        } else if (!isEmail(valor)) {
+            errorField.innerText = "El email no es válido";
+            field.focus();
+            return false;
+        } else {
+            errorField.innerText = "";
+            return true;
+        }
+    };
+    const validatePassField = (errorMessage) => {
+        const field = document.getElementById("pass");
+        const errorField = document.getElementById('errortext-pass');
+        const value = field.value.trim();
+
+        if (value === '') {
+            errorField.innerText = errorMessage;
+            return false;
+        } else {
+            errorField.innerText = "";
+            return true;
+        }
+    };
+    const validateConfirmPassField = (errorMessage) => {
+        const inputPassField = document.getElementById("pass");
+        const passFieldValue = inputPassField.value.trim();
+
+        const field = document.getElementById("confirm_pass");
+        const value = field.value.trim();
+        const errorField = document.getElementById('errortext-confirmarpass');
+
+
+        if (value === '') {
+            errorField.innerText = "";
+            return true
+        } else if (!(value === passFieldValue)) {
+            errorField.innerText = errorMessage;
+            return false;
+        } else {
+            errorField.innerText = "";
+            return true;
+        }
+    };
+    const validateAcceptTerms = () => {
+        const field = document.getElementById("accept");
+        const valor = field.checked;
+        const errorField = document.getElementById('errortext-aceptar');
+        // Si el campo de correo electrónico está vacío
+        if (!valor) {
+            errorField.innerText = "Tenés que aceptar los terminos";
+            field.focus();
+            return false;
+        } else {
+            errorField.innerText = "";
+            return true;
+        }
+
+    };
+
+
+    // Función para validar si una cadena es una dirección de correo electrónico válida
+    const isEmail = (email) => {
+        // Expresión regular para validar el formato de correo electrónico
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        // Verifica si el correo electrónico cumple con el formato
+        return re.test(email);
+    };
+});
